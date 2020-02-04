2cd7474aadbdaeacc838d335b58708918ed14582
YufenLin <yufenlin33@gmail.com>
2020-02-04 12:51:58 -0800

    return similar user's top 5

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from waitress import serve
 from src.models.wine_predictor import predict_wine
 
 from src.models.sommelier import check_user_exist, is_similar_user
-from src.utils import validate_input
+from src.utils import validate_input_wine, validate_input_user
 
 app = Flask(__name__, static_url_path="/static")
 
@@ -12,16 +12,16 @@ def index():
     """Return the main page."""
     return send_from_directory("static", "index.html")
 
-@app.route("/get_results", methods=["POST"])
-def get_results():
+@app.route("/get_wine", methods=["POST"])
+def get_wine():
     """ Predict the class of wine based on the inputs. """
     data = request.form
-    print(data)
-    
-    print(data['user_id'])
-    test_value, errors = validate_input(data)
+    # print(data) 
+    # print(data['user_id'])
+    # test_value, errors = validate_input_wine(data)
 
-    if not errors:
+    # if not errors:
+    if True:
         # predicted_class = predict_wine(test_value)
         user_exist = check_user_exist(data['user_id'])
         
@@ -34,8 +34,36 @@ def get_results():
                 return render_template("user_exist_wo_similar.html", user_id=user_id)
         else:
             return render_template("new_user.html")
-    else:
-        return abort(400, errors)
+    # else:
+    #     return abort(400, errors)
+
+
+@app.route("/get_results", methods=["POST"])
+def get_results():
+    """ Predict the class of wine based on the inputs. """
+    data = request.form
+    # print(data) 
+    # print(data['user_id'])
+    # test_value, errors = validate_input_user(data)
+    # if not errors:
+    if True:
+        # predicted_class = predict_wine(test_value)
+        user_exist = check_user_exist(data['user_id'])
+        
+        if user_exist:
+            user_id = int(data['user_id'])
+            similar_user, similar_user_top = is_similar_user(user_id)
+
+            if similar_user:
+                return render_template("user_exist.html", user_id=user_id, similar_user=similar_user, tables=[similar_user_top.to_html(classes='data')], titles=similar_user_top.columns.values)
+            else:
+                return render_template("user_exist_wo_similar.html", user_id=user_id)
+        else:
+            return render_template("new_user.html")
+    # else:
+    #     return abort(400, errors)
+
+    
 
 if __name__ == "__main__":
     serve(app, host='0.0.0.0', port=5000)
--- src/models/sommelier.py
+++ src/models/sommelier.py
@@ -20,3 +20,26 @@ def load_model():
     # loaded_model = pickle.load(model_file)
     # model_file.close()
     return data
+
+def is_similar_user(in_user_id):
+    wine_user = load_model()
+    # There is error need to be fixed.
+    wine_user_rating = wine_user.pivot_table(index = ["wine_id"], columns = ['taster_id'], values = "points")
+    similarity_with_other_user = wine_user_rating.corrwith(wine_user_rating[in_user_id]) 
+    similarity_with_other_user = similarity_with_other_user.sort_values(ascending=False)
+
+    # similarity_user = similarity_with_other_user[similarity_with_other_user.index!=in_user_id]
+    if similarity_with_other_user.values[1]>0.8:
+        # print(similarity_with_other_user.index[1])
+        similar_user = similarity_with_other_user.index[1]
+        similar_user_top = get_user_top_n(similar_user, wine_user, n=5)
+        
+        return similar_user, similar_user_top
+    else:
+        print("No similar taste user.")
+        return False
+    # 7 - 19 
+    # 8 >< 15
+    # 14 - 16
+def get_user_top_n(user_id, wine_user, n=5):
+    return wine_user[wine_user['taster_id']==user_id].sort_values(by=['points'], ascending=False)[0:5][['wine_id','title']]
