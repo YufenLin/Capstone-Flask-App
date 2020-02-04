b9a97e476c220047c75d12edc8663c7cb062ef71
YufenLin <yufenlin33@gmail.com>
2020-02-03 17:49:26 -0800

    return similar taste user

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, send_from_directory, render_template, request, abort
 from waitress import serve
 from src.models.wine_predictor import predict_wine
 
-from src.models.sommelier import check_user_exist
+from src.models.sommelier import check_user_exist, is_similar_user
 from src.utils import validate_input
 
 app = Flask(__name__, static_url_path="/static")
@@ -24,8 +24,14 @@ def get_results():
     if not errors:
         # predicted_class = predict_wine(test_value)
         user_exist = check_user_exist(data['user_id'])
+        
         if user_exist:
-            return render_template("user_exist.html", user_id=user_exist)
+            user_id = int(data['user_id'])
+            similar_user = is_similar_user(user_id)
+            if similar_user:
+                return render_template("user_exist.html", user_id=user_id, similar_user=similar_user)
+            else:
+                return render_template("user_exist_wo_similar.html", user_id=user_id)
         else:
             return render_template("new_user.html")
     else:
