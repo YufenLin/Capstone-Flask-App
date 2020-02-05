ce52415769e7fd5e2a2052ba173718a0c5aa24d9
YufenLin <yufenlin33@gmail.com>
2020-02-05 09:57:04 -0800

    add wine_train.pkl and style.css

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, send_from_directory, render_template, request, abort
 from waitress import serve
 from src.models.wine_predictor import predict_wine
 
-from src.models.sommelier import check_user_exist, is_similar_user
+from src.models.sommelier import check_user_exist, have_similar_user
 from src.utils import validate_input_wine, validate_input_user
 
 app = Flask(__name__, static_url_path="/static")
@@ -18,22 +18,26 @@ def get_wine():
     data = request.form
     # print(data) 
     # print(data['user_id'])
-    # test_value, errors = validate_input_wine(data)
-
+    # test_value, errors = validate_input_user(data)
     # if not errors:
-    if True:
-        # predicted_class = predict_wine(test_value)
-        user_exist = check_user_exist(data['user_id'])
-        
-        if user_exist:
-            user_id = int(data['user_id'])
-            similar_user = is_similar_user(user_id)
-            if similar_user:
-                return render_template("user_exist.html", user_id=user_id, similar_user=similar_user)
-            else:
-                return render_template("user_exist_wo_similar.html", user_id=user_id)
-        else:
-            return render_template("new_user.html")
+    wine_id = int(data['wine_id'])
+    
+    # tfidf_recommend_list = recommend_wine(wine_train, 89368, n=5, cosine_sim = test)
+
+    # if True:
+    #     # predicted_class = predict_wine(test_value)
+    #     user_exist = check_user_exist(data['user_id'])
+    #     if user_exist:
+    #         user_id = int(data['user_id'])
+            
+    #         is_similar_user, similar_user, top_n = have_similar_user(user_id)
+    #         if is_similar_user:
+    #             return render_template("user_exist.html", user_id=user_id, similar_user=similar_user, tables=[top_n.to_html(classes='male')], columns=top_n.columns.values)   
+    #         else:
+    #             return render_template("user_exist_wo_similar.html", user_id=user_id, tables=[top_n.to_html(classes='male')], columns=top_n.columns.values)
+    #     else:
+    #         return render_template("new_user.html")
+
     # else:
     #     return abort(400, errors)
 
@@ -49,15 +53,13 @@ def get_results():
     if True:
         # predicted_class = predict_wine(test_value)
         user_exist = check_user_exist(data['user_id'])
-        
         if user_exist:
             user_id = int(data['user_id'])
-            similar_user, similar_user_top = is_similar_user(user_id)
-
-            if similar_user:
-                return render_template("user_exist.html", user_id=user_id, similar_user=similar_user, tables=[similar_user_top.to_html(classes='data')], titles=similar_user_top.columns.values)
+            is_similar_user, similar_user, top_n = have_similar_user(user_id)
+            if is_similar_user:
+                return render_template("user_exist.html", user_id=user_id, similar_user=similar_user, tables=[top_n.to_html(classes='male')], columns=top_n.columns.values)   
             else:
-                return render_template("user_exist_wo_similar.html", user_id=user_id)
+                return render_template("user_exist_wo_similar.html", user_id=user_id, tables=[top_n.to_html(classes='male')], columns=top_n.columns.values)
         else:
             return render_template("new_user.html")
     # else:
